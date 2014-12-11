72f412b52f8e908fd147bcc38cb956002b63fd79
kozo yamagata <tune002@gmail.com>
2014-12-11 16:45:12 +0900

    Implement collectionView and itemView

--- classes/2014-12-11-js/app.js
+++ classes/2014-12-11-js/app.js
@@ -15,6 +15,8 @@
       this.items = new Frontend.CarouselCollection(this.$el.data('items'));
       this.itemsView = new Frontend.CarouselCollectionView({ collection: this.items });
       this.bulletsView = new Frontend.CarouselBulletCollectionView({ collection: this.items });
+
+      this.items.first().set('active', true);
     }
   });
 
@@ -23,25 +25,66 @@
     defaults: {
       label: '',
       url: '',
-      active: false
-    }
+      active: false,
+      num: 0
+    },
   });
 
   Frontend.CarouselCollection = Backbone.Collection.extend({
-    model: Frontend.Carousel
+    model: Frontend.Carousel,
+
+    setAllNum: function() {
+      this.each(function(model, i) {
+        model.set('num', i + 1);
+      });
+    }
   });
   //= model end
 
   //= item view begin
-  Frontend.CarouselItemCollectionView = Backbone.View.extend({
+  Frontend.CarouselCollectionView = Backbone.View.extend({
     initialize: function(options) {
       this.options = options;
+      this.setElement(document.querySelector('[data-role="carousel-items"]'));
+      var self = this;
+
+      this.collection.setAllNum();
+      this.collection.each(function(model, i) {
+        var itemView = new Frontend.CarouselItemView({ model: model });
+        self.$el.append(itemView.render().$el);
+      });
     }
   });
 
   Frontend.CarouselItemView = Backbone.View.extend({
+    template: function() {
+      return _.template($('#carousel-item-template').html());
+    },
+
     initialize: function(options) {
       this.options = options;
+      var self = this;
+
+      this.listenTo(this.model, 'change', function() {
+        if (self.model.get('active')) {
+          self.toActive();
+        } else {
+          self.toInactive();
+        }
+      });
+    },
+
+    render: function() {
+      this.$el = $(this.template()(this.model.toJSON()));
+      return this;
+    },
+
+    toActive: function() {
+      this.$el.addClass('active');
+    },
+
+    toInactive: function() {
+      this.$el.removeClass('active');
     }
   });
   //= item view end
