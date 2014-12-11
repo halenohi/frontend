87bbd718a06ca2862a2fc0ef51dd25fab4a1d6fd
kozo yamagata <tune002@gmail.com>
2014-12-11 16:23:04 +0900

    Implement carousel module basic

---
+++ classes/2014-12-11-js/app.js
@@ -0,0 +1,74 @@
+(function() {
+
+  // Application namespace
+  window.Frontend = {};
+
+  // Bootstrap function
+  Frontend.initialize = function() {
+    new Frontend.CarouselView();
+  };
+
+  Frontend.CarouselView = Backbone.View.extend({
+    initialize: function() {
+      this.setElement(document.querySelector('[data-role="carousel"]'));
+
+      this.items = new Frontend.CarouselCollection(this.$el.data('items'));
+      this.itemsView = new Frontend.CarouselCollectionView({ collection: this.items });
+      this.bulletsView = new Frontend.CarouselBulletCollectionView({ collection: this.items });
+    }
+  });
+
+  //= model begin
+  Frontend.Carousel = Backbone.Model.extend({
+    defaults: {
+      label: '',
+      url: '',
+      active: false
+    }
+  });
+
+  Frontend.CarouselCollection = Backbone.Collection.extend({
+    model: Frontend.Carousel
+  });
+  //= model end
+
+  //= item view begin
+  Frontend.CarouselItemCollectionView = Backbone.View.extend({
+    initialize: function(options) {
+      this.options = options;
+    }
+  });
+
+  Frontend.CarouselItemView = Backbone.View.extend({
+    initialize: function(options) {
+      this.options = options;
+    }
+  });
+  //= item view end
+
+  //= control view
+  Frontend.CarouselControl = Backbone.View.extend({
+    initialize: function(options) {
+      this.options = options;
+    }
+  });
+
+  //= bullet view begin
+  Frontend.CarouselBulletCollectionView = Backbone.View.extend({
+    initialize: function(options) {
+      this.options = options;
+    }
+  });
+
+  Frontend.CarouselBulletView = Backbone.View.extend({
+    initialize: function(options) {
+      this.options = options;
+    }
+  });
+  //= bullet view end
+
+  // DOM Ready
+  jQuery(function($) {
+    Frontend.initialize();
+  });
+}).call(this)
